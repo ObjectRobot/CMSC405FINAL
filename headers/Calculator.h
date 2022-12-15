7c0bea68e4e813e5ce37177f4875091790eba7c9
Oliver Kokolee <ollie12245@gmail.com>
2022-12-15 13:36:08 -0500

    CPlusPLusFinal

---
+++ headers/Calculator.h
@@ -0,0 +1,11 @@
+// include pre-processor directives
+#include <iostream>
+
+// use C++ standard library namespace
+using namespace std;
+// Question 1
+// declare the class
+class Calculator
+{
+    public: virtual void calculate() = 0;
+};
